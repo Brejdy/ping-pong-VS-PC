1e5fb28fb4f797a05fb9fd82559b2e35e02fe2b5
Brejdy <jan.j.bradac@seznam.cz>
2025-05-09 15:15:10 +0200

    Add project files.

---
+++ ping pong app/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ping_pong_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.Computer = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Computer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.Image = global::ping_pong_app.Properties.Resources.Solid_white_bordered_svg;
+            this.Player.Location = new System.Drawing.Point(12, 151);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(30, 120);
+            this.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // Computer
+            // 
+            this.Computer.Image = global::ping_pong_app.Properties.Resources.Solid_white_bordered_svg;
+            this.Computer.Location = new System.Drawing.Point(758, 151);
+            this.Computer.Name = "Computer";
+            this.Computer.Size = new System.Drawing.Size(30, 120);
+            this.Computer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Computer.TabIndex = 0;
+            this.Computer.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.Image = global::ping_pong_app.Properties.Resources.Table_tennis_ball;
+            this.ball.Location = new System.Drawing.Point(372, 199);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(30, 30);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Computer);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.Player);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Player : 0 --- Computer: 0";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Computer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.PictureBox Computer;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
---
+++ ping pong app/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Media;
+
+namespace ping_pong_app
+{
+    public partial class Form1 : Form
+    {
+        //initial ball speed 
+        int ballXspeed = 4;
+        int ballYspeed = 4;
+
+        //initial pc speed
+        int speed = 2;
+
+        Random rand = new Random();
+
+        //player going up or down check
+        bool goDown, goUp;
+
+        //pc speed change interval
+        int computerSpeedChange = 50;
+        int playerScore = 0;
+        int computerScore = 0;
+        int playerSpeed = 8;
+
+        //speed for ball and pc
+        int[] i = { 5, 6, 8, 9 };
+        int[] j = { 10, 8, 8, 11, 12 };
+
+        //zvuky
+        SoundPlayer racket = new SoundPlayer(@"..\ping pong app\racket.wav");
+        SoundPlayer ground = new SoundPlayer(@"..\ping pong app\ground.wav");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            //ball movement
+            ball.Top -= ballYspeed;
+            ball.Left -= ballXspeed;
+
+            //score in header
+            this.Text = "Player Score: " + playerScore + " --- Computer Score: " + computerScore;
+
+            //bounce off wall
+            if (ball.Top < 0 || ball.Bottom > this.ClientSize.Height)
+            {
+                ballYspeed = -ballYspeed;
+                ground.Play();
+            }
+
+            //player missed
+            if (ball.Left < -2)
+            {
+                ball.Left = 300;
+                ballXspeed = -ballXspeed;
+                computerScore++;
+            }
+
+            //pc missed
+            if (ball.Right > this.ClientSize.Width + 2)
+            {
+                ball.Left = 300;
+                ballXspeed = -ballXspeed;
+                playerScore++;
+            }
+
+            //computer cant leave screen
+            if (Computer.Top <= 1)
+            {
+                Computer.Top = 0;
+            }
+            else if (Computer.Bottom >= this.ClientSize.Height)
+            {
+                Computer.Top = this.ClientSize.Height - Computer.Height;
+            }
+
+            //computer moving up and down
+            if (ball.Top < Computer.Top + (Computer.Height / 2) && ball.Left > 300)
+            {
+                Computer.Top -= speed;
+            }
+            if (ball.Top > Computer.Top + (Computer.Height / 2) && ball.Left > 300)
+            {
+                Computer.Top += speed;
+            }
+
+            computerSpeedChange -= 1;
+            if(computerSpeedChange < 0)
+            {
+                speed = i[rand.Next(i.Length)];
+                computerSpeedChange = 50;
+            }
+
+            //player cant leave the screen
+            if (goDown && Player.Top + Player.Height < this.ClientSize.Height)
+            {
+                Player.Top += playerSpeed;
+            }
+
+            if (goUp && Player.Top > 0)
+            {
+                Player.Top -= playerSpeed;
+            }
+
+            //collision control
+            CheckCollision(ball, Player, Player.Right + 5);
+            CheckCollision(ball, Computer, Computer.Left - 35);
+
+
+            //game over
+            if (computerScore == 5)
+            {
+                GameOver("Sorry you lost THE GAME");
+            }
+            if (playerScore == 5)
+            {
+                GameOver("You won this time!");
+            }
+        }
+
+        //arrow pressing
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+            if(e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+            }
+        }
+
+        //arrow release
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+        }
+
+        //2 object colision
+        private void CheckCollision(PictureBox picOne, PictureBox picTwo, int offset)
+        {
+            if (picOne.Bounds.IntersectsWith(picTwo.Bounds))
+            {
+                picOne.Left = offset;
+
+                int x = j[rand.Next(j.Length)];
+                int y = j[rand.Next(j.Length)];
+
+                if (ballXspeed < 0)
+                {
+                    ballXspeed = x;
+                }
+                else
+                {
+                    ballXspeed = -x;
+                }
+
+                if (ballYspeed < 0)
+                {
+                    ballYspeed = -y;
+                    racket.Play();
+                }
+                else
+                {
+                    ballYspeed = y;
+                    racket.Play();
+                }
+
+            }
+        }
+
+        //Game Over window
+        private void GameOver(string Message)
+        {
+            GameTimer.Stop();
+            MessageBox.Show(Message, "Game Says: ");
+        }
+    }
+}
